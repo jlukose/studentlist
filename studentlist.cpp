0d55d9edd108934c91fee1f171c06dc8d3e8781a
jlukose <71291290+jlukose@users.noreply.github.com>
2020-10-18 20:57:13 -0700

    Add files via upload

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,63 +1,79 @@
 #include<iostream>
-#include<string>
+#include<cstring>
+#include<vector>
+#include<iterator>
+#include<iomanip>
 
 using namespace std;
 
-struct student{
+struct S{
   char firstname[10];
   char lastname[10];
   int id;
   float gpa;
 };
 
-std::string input;
-string str;
-
-void studentdelete(int a);
-
-void studentadd();
-
-void studentprint();
+char input[6];
+void studentdelete(vector<S*>* StudentList, int deleteid);
+S* studentadd();
+void studentprint(vector<S*> StudentList);
 
 int main(){
   cout << "Welcome to student list, please type 'ADD' to add a student, 'PRINT' to print all students stored, 'DELETE' to delete a student, and 'QUIT' to quit." << endl;
+  S List;
+  vector<S*> StudentList;
   while(true){
+    cout << "Command list: ADD  PRINT  DELETE  QUIT" << endl;
+    for (int i = 0; i < 7; i++){
+      input[i] = '\0';
+    }
     cin >> input;
-    str = "QUIT";
-    std::size_t found = input.find(str);
-    if (found != std::string::npos){
-      cout << "Quitting..." << endl;
+    if (strcmp(input, "QUIT") == 0){
       return 0;
     }
-    str = "ADD";
-    found = input.find(str);
-    if (found != std::string::npos){
-      studentadd();
+    if (strcmp(input, "ADD") == 0){
+      StudentList.push_back(studentadd());
     }
-    str = "PRINT";
-    found = input.find(str);
-    if (found != std::string::npos){
-      studentprint();
+    if (strcmp(input, "PRINT") == 0){
+      studentprint(StudentList);
     }
-    str = "DELETE";
-    found = input.find(str);
-    if (found != std::string::npos){
+    if (strcmp(input, "DELETE") == 0){
       cout << "Please enter the Student ID that you wish to delete" << endl;
-      int deletenumber;
-      cin >> deletenumber;
-      studentdelete(deletenumber);
+      int deleteid;
+      cin >> deleteid;
+      studentdelete(&StudentList, deleteid);
     }
+    cin.clear();
+    cin.ignore(10000000, '\n');
   }
 }
 
-void studentadd(){
-
+S* studentadd(){
+  S* newstudent = new S();
+  cout << "Enter first name" << endl;
+  cin >> newstudent->firstname;
+  cin.clear();
+  cin.ignore(10000000, '\n');
+  cout << "Enter last name" << endl;
+  cin >> newstudent->lastname;
+  cin.clear();
+  cin.ignore(10000000, '\n');
+  cout << "Enter ID" << endl;
+  cin >> newstudent->id;
+  cin.clear();
+  cin.ignore(10000000, '\n');
+  cout << "Enter GPA" << endl;
+  cin >> newstudent->gpa;
+  return newstudent;
 }
 
-void studentprint(){
-
+void studentprint(vector<S*> StudentList){
+  vector<S*>::iterator ptr;
+  for (ptr = StudentList.begin(); ptr < StudentList.end(); ptr++){
+    cout << (*ptr)->firstname << " " << (*ptr)->lastname << ", " << (*ptr)->id << setprecision(3) << ", " << (*ptr)->gpa << endl;
+  }
 }
 
-void studentdelete(int a){
+void studentdelete(vector<S*>* StudentList, int deleteid){
   
 }
